ac4a134e3510c20fa86fe537f5b967ccc8779fa6
Giovanni Mendes Mansueto <topogigiovanni@gmail.com>
2016-10-23 20:47:12 -0200

    improve confirmation choices and dialog

--- test/test.bat
+++ test/test.bat
@@ -1,27 +1,20 @@
 @ECHO OFF
+SETLOCAL EnableDelayedExpansion
 ::references
 REM http://stackoverflow.com/questions/7425360/batch-file-to-search-for-a-string-within-directory-names
 ::variables
-SETLOCAL EnableDelayedExpansion
-SET validpathterm=.corecommerce
 SET destroot=.\dest
 
 FOR /f "delims=" %%i IN ( ' dir /ad/b "%destroot%"' ) DO (
-	REM SET /a str1=eu
-	REM Set DWRPATH="%%i
-	REM if not x%str1:bcd=%==x%str1% (
-		REM robocopy ".\origin" "%destroot%\%%i" /IS /s 
-		REM ECHO "copiado em %destroot%\%%i %str1%" 
-		 REM ECHO "copiado em %%i\%str1%" 
-	REM ) 
-	
 
 	REM echo "%%i"
 	SET "folder=%%i"
 	if /I NOT "!folder:corecommerce=!"=="!folder!" (
+		REM robocopy ".\origin" "%destroot%\%%i" /IS /s 
 		ECHO "copiado em %%i" 
 	)
    REM set AntiVirus1="adobe"
+   
 )
 
 REM echo %AntiVirus1% found
